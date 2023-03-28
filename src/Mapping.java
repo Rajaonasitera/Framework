94b8df0c03f09c3fb8870dbc00e788e19aed81b2
Tatinaa <tarajaonasitera@gmail.com>
2023-03-28 10:01:40 +0200

    Sprint3
    hashmap

--- src/Mapping.java
+++
@@ -1,5 +0,0 @@
-package etu1814.framework;
-public class Mapping {
-    String className;
-    String Method;
-}
---
+++ src/test/test.java
@@ -0,0 +1,20 @@
+package test;
+
+import etu1814.framework.Util.Url;
+
+/**
+ * test1
+ */
+public class test {
+
+    @Url(url = "ajout")
+    public void Ajout(){
+
+    }
+
+    public void huhu(){
+
+    }
+
+
+}
